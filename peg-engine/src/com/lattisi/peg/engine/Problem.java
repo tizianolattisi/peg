1f8cceb8a6ecd22edc60262196a8882d782652d3
Tiziano Lattisi <tiziano@axiastudio.it>
2013-12-16 22:19:00 +0100

    Il find ora cerca anche gli item con un nome appartenente alla permutazione dei punti.

--- peg-engine/src/com/lattisi/peg/engine/Problem.java
+++ peg-engine/src/com/lattisi/peg/engine/Problem.java
@@ -3,8 +3,7 @@ package com.lattisi.peg.engine;
 import com.lattisi.peg.engine.entities.IContainer;
 import com.lattisi.peg.engine.entities.IItem;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * User: tiziano
@@ -24,6 +23,10 @@ public class Problem {
     }
 
 
+    public static IItem find(String name){
+        return find(name, null);
+    }
+
     public static IItem find(String name, Class klass){
         IItem found = null;
         for( IItem item: items.values() ){
@@ -41,9 +44,10 @@ public class Problem {
     // TODO: implements klass parameter
     private static IItem scan(IItem item, String name, Class klass){
         IItem found = null;
+        Collection<String> aliases = resolveAliases(name);
         if( item instanceof IContainer ){
             for( IItem child: ((IContainer) item).getChildren() ){
-                if( child.getName().equals(name) ){
+                if( aliases.contains(child.getName()) ){
                     return child;
                 }
                 found = scan(child, name, klass);
@@ -51,4 +55,24 @@ public class Problem {
         }
         return found;
     }
+
+    private static Collection<String> resolveAliases(String itemName){
+        Collection<String> aliases = new ArrayList<String>();
+        aliases.add(itemName);
+        if( itemName.length() == 2 ){
+            StringBuilder sb = new StringBuilder(itemName);
+            aliases.add(sb.reverse().toString());
+        } else if( itemName.length() == 3 ){
+            String a = itemName.substring(0, 1);
+            String b = itemName.substring(1, 2);
+            String c = itemName.substring(2, 3);
+            aliases.add(a+c+b);
+            aliases.add(b+a+c);
+            aliases.add(b+c+a);
+            aliases.add(c+a+b);
+            aliases.add(c+b+a);
+        }
+        return aliases;
+    }
+
 }
