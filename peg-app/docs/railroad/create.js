d940c5761b444be2e969e82fb405d8f9530c3863
Tiziano Lattisi <tiziano@axiastudio.it>
2014-01-17 22:50:49 +0100

    Esempio di documentazione della dsl tramite railroad.

---
+++ peg-app/docs/railroad/create.js
@@ -0,0 +1,39 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: tiziano
+ * Date: 17/01/14
+ * Time: 22:31
+ * To change this template use File | Settings | File Templates.
+ */
+
+Diagram(
+
+    NonTerminal('create'),
+
+    Choice(2,
+        NonTerminal('triangle'),
+        NonTerminal('segment'),
+        NonTerminal('point'),
+        NonTerminal('angle'),
+        NonTerminal('direction')
+    ),
+
+    NonTerminal('name'),
+    Terminal('"item-name"'),
+
+    Optional(
+        Sequence(
+            NonTerminal('with'),
+            OneOrMore(
+                Sequence(
+                    Choice(1,
+                        Sequence(NonTerminal('type:'), Terminal('"type"')),
+                        Sequence(NonTerminal('measure:'), Terminal('"measure-string"'))
+                    ),
+                    Optional(",")
+                )
+            )
+        )
+    )
+
+)
