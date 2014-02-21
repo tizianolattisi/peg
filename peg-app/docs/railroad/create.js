adeefd39fbba9137fa79430feb7ee647dc4316e7
Tiziano Lattisi <tiziano@axiastudio.it>
2014-02-21 16:24:14 +0100

    Modifiche al railroad (create).

--- peg-app/docs/railroad/create.js
+++ peg-app/docs/railroad/create.js
@@ -6,6 +6,7 @@
  * To change this template use File | Settings | File Templates.
  */
 
+var out =
 Diagram(
 
     NonTerminal('create'),
@@ -35,5 +36,6 @@ Diagram(
             )
         )
     )
+);
 
-)
+document.write(out);
